a2030fe5ac6ebdff5342a34c0851274f3a45eaf4
Jose <davidherrera0429@gmail.com>
2020-11-28 02:39:30 -0500

    crud de vehiculos completo

--- src/componets/VehiculoModal.js
+++ src/componets/VehiculoModal.js
@@ -1,14 +1,229 @@
-import React from "react";
-import { Modal } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Modal, FormGroup, FormLabel, FormControl, Button } from "react-bootstrap";
+import ClientesService from "../services/ClientesService";
+import VehiculosService from "../services/VehiculosService";
+import Swal from 'sweetalert2';
 
-function VehiculoModal (props) {
+function VehiculoModal(props) {
 
-    const {show, handleClose, handleGetVehiculos, vehiculo, accion} = props
+    const { show, handleClose, handleGetVehiculos, vehiculo, accion } = props
+
+    useEffect(() => {
+        handleGetClientes();
+    }, [])
+
+    //Clientes para llenar la caja de texto
+    const [clientes, setClientes] = useState(null);
+
+
+    //Variables para actualizar 
+    const [placa, setPlaca] = useState(accion === "editar" ? vehiculo.placa : null);
+    const [nombre, setNombre] = useState(accion === "editar" ? vehiculo.nombre : null);
+    const [marca, setMarca] = useState(accion === "editar" ? vehiculo.marca : null);
+    const [modelo, setModelo] = useState(accion === "editar" ? vehiculo.modelo : null);
+    const [color, setColor] = useState(accion === "editar" ? vehiculo.color : null);
+    const [dueño, setDueño] = useState(accion === "editar" ? vehiculo.dueño : null);
+    const [imagen, setImagen] = useState(accion === "editar" ? vehiculo.image : null);
+
+
+    const handleGetClientes = async () => {
+        try {
+            const resp = await ClientesService.get();
+            setClientes(resp.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const handleCrearVehiculo = async () => {
+        try {
+            const resp = await VehiculosService.post({
+                placa: placa,
+                nombre: nombre,
+                marca: marca,
+                modelo: modelo,
+                color: color,
+                dueño: {
+                    ccCliente: parseInt(dueño)
+                },
+                image: imagen
+            })
+            await Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'Vehiculo Creado',
+                showConfirmButton: false,
+                timer: 1000
+            })
+            handleClose();
+            handleGetVehiculos();
+            console.log(resp);
+        } catch (error) {
+            console.log(error);
+            Swal.fire({
+                title: 'Error',
+                icon: 'error',
+                text: 'Se presentó un problema al crear el vehiculo',
+                timer: 5000
+            })
+        }
+    }
+
+    const handleActualizarVehiculo = async () => {
+        try {
+            const resp = await VehiculosService.put(placa, {
+                placa: placa,
+                nombre: nombre,
+                marca: marca,
+                modelo: modelo,
+                color: color,
+                dueño: {
+                    ccCliente: parseInt(dueño)
+                },
+                image: imagen
+            });
+            console.log(resp);
+            await Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'Vehiculo Actualizado',
+                showConfirmButton: false,
+                timer: 1000
+            })
+            handleClose();
+            handleGetVehiculos();
+        } catch (error) {
+            console.log(error);
+            Swal.fire({
+                title: 'Error',
+                icon: 'error',
+                text: 'Se presentó un problema al actualizar el vehiculo',
+                timer: 5000
+            })
+        }
+
+    }
+
+    const handleOnChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+
+        console.log(value);
+
+        switch (name) {
+            case "placa":
+                setPlaca(value ? value : null);
+                break;
+            case "nombre":
+                setNombre(value ? value : null);
+                break;
+            case "modelo":
+                setModelo(value ? value : null);
+                break;
+            case "marca":
+                setMarca(value ? value : null);
+                break;
+            case "color":
+                setColor(value ? value : null);
+                break;
+            case "dueño":
+                setDueño(value ? value : null);
+                break;
+            case "imagen":
+                setImagen(value ? value : null);
+                break;
+        }
+
+    }
+
+
     return (
-        <Modal backdrop="static" show={show} onHide = {handleClose}>
-             <Modal.Header closeButton>
-               <Modal.Title>{accion === "editar"? "Actualizar Vehiculo":"Crear Vehiculo"}</Modal.Title>
+        <Modal backdrop="static" show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>{accion === "editar" ? "Actualizar Vehiculo" : "Crear Vehiculo"}</Modal.Title>
             </Modal.Header>
+            <Modal.Body>
+                <FormGroup>
+                    <FormLabel>Placa</FormLabel>
+                    <FormControl
+                        disabled={accion == "editar" ? true : false}
+                        name="placa"
+                        onChange={handleOnChange}
+                        value={placa ? placa : ""}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <FormLabel>Nombre</FormLabel>
+                    <FormControl
+                        name="nombre"
+                        onChange={handleOnChange}
+                        value={nombre ? nombre : ""}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <FormLabel>Modelo</FormLabel>
+                    <FormControl
+                        name="modelo"
+                        onChange={handleOnChange}
+                        value={modelo ? modelo : ""}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <FormLabel>Marca</FormLabel>
+                    <FormControl
+                        name="marca"
+                        onChange={handleOnChange}
+                        value={marca ? marca : ""}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <FormLabel>Color</FormLabel>
+                    <FormControl
+                        name="color"
+                        onChange={handleOnChange}
+                        value={color ? color : ""}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <FormLabel>Dueño</FormLabel>
+                    <FormControl
+                        as="select"
+                        name="dueño"
+                        onChange={handleOnChange}
+                        value={dueño ? dueño : ""}
+                    >
+                        <option value={accion === "editar" ? dueño.ccCliente : ""}> {accion === "editar" ? dueño.nombre : "-SELECCIONE"}</option>
+                        {
+                            clientes
+                            && clientes.map((cliente, index) => {
+                                return <option key={index} value={cliente.ccCliente}>{cliente.nombre}</option>
+                            })
+                        }
+                    </FormControl>
+                </FormGroup>
+                <FormGroup>
+                    <FormLabel>Imagen</FormLabel>
+                    <FormControl
+                        name="imagen"
+                        onChange={handleOnChange}
+                        value={imagen ? imagen : ""}
+                    />
+                </FormGroup>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="danger" onClick={handleClose}>Cancelar</Button>
+                <Button variant="success" disabled = {
+                    !placa ||
+                    !nombre ||
+                    !modelo ||
+                    !marca ||
+                    !color ||
+                    !dueño ||
+                    !imagen
+                }
+                onClick={accion === "editar" ? handleActualizarVehiculo : handleCrearVehiculo}>{accion === "editar" ? "Actualizar" : "Crear"}</Button>
+            </Modal.Footer>
+
         </Modal>
     )
 
--- src/views/VehiculosView.js
+++ src/views/VehiculosView.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import { Button, CardDeck, Container } from "react-bootstrap";
+import { Button, CardDeck, Container, FormControl, InputGroup } from "react-bootstrap";
 
 import VehiculosServicie from "../services/VehiculosService";
 
@@ -8,15 +8,17 @@ import CardVehiculo from "../componets/CardVehiculo";
 
 import VehiculoModal from "../componets/VehiculoModal";
 
+import Swal from 'sweetalert2';
+
 
 
 function VehiculosView() {
 
     const [vehiculos, setVehiculos] = useState(null);
 
-    const[show, setShow] = useState(false);
+    const [show, setShow] = useState(false);
 
-    const [accion,setAccion] = useState(null);
+    const [accion, setAccion] = useState(null);
 
     const [vehiculoActualizar, setVehiculoActualizar] = useState(null);
 
@@ -26,15 +28,45 @@ function VehiculosView() {
 
     const handleGetVehiculos = async () => {
         try {
+            Swal.fire({
+                allowOutsideClick: false,
+                icon: 'info',
+                text: 'Por favor espere...',
+                timer: 10000
+            });
+
+            Swal.showLoading();
             const resp = await VehiculosServicie.get();
             setVehiculos(resp.data);
+            Swal.close();
         } catch (error) {
             console.log(error);
+            Swal.close();
+            Swal.fire({
+                title: 'Error',
+                icon: 'error',
+                text: 'Se presentó un error al obtener los Vehiculos',
+                timer: 5000
+            })
         }
     }
 
     const handleDeleteVehiculo = async (placa) => {
         try {
+            const resModal = await Swal.fire({
+                title: 'Se eliminará el vehiculo',
+                icon: 'info',
+                text: '¿Esta Seguro de eliminar el vehiculo?',
+                showCancelButton: true,
+                confirmButtonText: 'Si',
+                cancelButtonText: 'Cancelar',
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+            });
+            if (!resModal.value) {
+                return;
+            }
+
             const resp = await VehiculosServicie.delete(placa);
             console.log(resp);
             handleGetVehiculos();
@@ -43,9 +75,9 @@ function VehiculosView() {
         }
     }
 
-    const handleVehiculoActualizar = (vehiculoActualizar) =>{
+    const handleVehiculoActualizar = (vehiculoActualizar) => {
         handleOpenModal("editar");
-        console.log(vehiculoActualizar);
+        setVehiculoActualizar(vehiculoActualizar);
     }
 
     //metodo para cerrar modal 
@@ -74,27 +106,27 @@ function VehiculosView() {
 
         const arrayRows = [...Array(rows)];
 
-        return arrayRows.map((row,index) =>{
+        return arrayRows.map((row, index) => {
             return (
-                <CardDeck key = {index}>
+                <CardDeck key={index}>
                     {
                         vehiculos.slice(
-                            index === 0 ? index: index*columns,
-                            index === 0 ? columns : index *columns + columns
-                        ).map((vehiculo,index)=>{
+                            index === 0 ? index : index * columns,
+                            index === 0 ? columns : index * columns + columns
+                        ).map((vehiculo, index) => {
                             return <CardVehiculo
-                            key = {index}
-                            placa = {vehiculo.placa}
-                            nombre = {vehiculo.nombre}
-                            modelo = {vehiculo.modelo}
-                            marca = {vehiculo.marca}
-                            color = {vehiculo.color}
-                            image = {vehiculo.image}
-                            dueño = {vehiculo.dueño}
-                            handleDeleteVehiculo = {handleDeleteVehiculo}
-                            handleVehiculoActualizar = {handleVehiculoActualizar}
+                                key={index}
+                                placa={vehiculo.placa}
+                                nombre={vehiculo.nombre}
+                                modelo={vehiculo.modelo}
+                                marca={vehiculo.marca}
+                                color={vehiculo.color}
+                                image={vehiculo.image}
+                                dueño={vehiculo.dueño}
+                                handleDeleteVehiculo={handleDeleteVehiculo}
+                                handleVehiculoActualizar={handleVehiculoActualizar}
 
-                             />
+                            />
                         })
                     }
 
@@ -105,20 +137,23 @@ function VehiculosView() {
 
     }
 
+
+
     return (
         <Container>
-            <div style = {{margin:"5px"}}>
-              <Button variant="outline-success" size="lg" block onClick = {() =>handleOpenModal("crear")} >Crea un nuevo vehiculo</Button>
-              {
-                  show
-                  && <VehiculoModal 
-                  show = {show}
-                  handleClose = {handleClose}
-                  handleGetVehiculos = {handleGetVehiculos}
-                  vehiculo = {vehiculoActualizar}
-                  accion = {accion} 
-                  />
-              }
+            <div style={{ marginTop: "20px" }}>
+                <Button variant="success" onClick={() => handleOpenModal("crear")} >Crea un nuevo vehiculo</Button>
+                {
+                    show ?
+                        <VehiculoModal
+                            show={show}
+                            handleClose={handleClose}
+                            handleGetVehiculos={handleGetVehiculos}
+                            vehiculo={vehiculoActualizar}
+                            accion={accion}
+                        /> : null
+
+                }
             </div>
             {handleRenderVehiculo()}
         </Container>
@@ -126,4 +161,5 @@ function VehiculosView() {
 
 }
 
-export default VehiculosView; 
+export default VehiculosView;
+
